415b7459cd538d69c2e9f9b0515e4df3ade7749c
fractionofadot <robert@fractionofadot.org>
2025-01-09 07:06:44 -0800

    Class based

---
+++ frontend/single-device-class.js
@@ -0,0 +1,32 @@
+class MovieNightDB {
+  constructor() {
+    this.db = {users: [{id: 0, name: "defaultUser"}], movies: [{id: 0, name: "The Princess Bride"}]};
+    this.max_name_length = 32;
+  }
+
+  addUser(username) {
+
+  }
+
+  removeUser(id_to_remove) {
+
+  }
+
+  addMovie(movie_title) {
+
+  }
+
+  removeMovie(id_to_remove) {
+
+  }
+
+  renameUser(user_id, new_username) {
+
+  }
+
+  renameMovie(movie_id, new_title) {
+
+  }
+
+
+}
