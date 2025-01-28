4db0dc201d4fef194f7a3b2e865fa8d76d7bf6a5
fractionofadot <robert@fractionofadot.org>
2025-01-28 14:35:58 -0800

    New data structure and methods. User add

--- frontend/MovieNightDB.js
+++ frontend/MovieNightDB.js
@@ -1,119 +1,173 @@
 class MovieNightDB {
   constructor() {
-    this.db = {users: [{id: 0, name: "defaultUser", scores: {0: 0}}], movies: [{id: 0, title: "The Princess Bride"}]};
+    this.users = {0: {
+      name: "Default User", 
+      scores: {0: 0} 
+    } };
+    this.movies = {0: {
+      title: "The Princess Bride", 
+      scores: {0: 0}
+    } };    
     this.max_name_length = 32;
     this.max_users = 10;
     this.max_movies = 20;
+    this.next_id = 1;
+  }
+
+  save() {
+    window.localStorage.setItem("MDBUsers", this.users);
+    window.localStorage.setItem("MDBMovies", this.movies);
+  }
+
+
+
+//// Get
+
+  get nextId() {
+    return this.next_id++;
+  }
+
+  get userIdList() {
+    return Object.keys(this.users);
+  }
+
+  get movieIdList() {
+    return Object.keys(this.movies);
+  }
+
+  get allUsers() {
+    return this.users;
+  }
+
+  get defaultScores() {
+    let user_id = Object.keys(this.users)[0]
+    return this.users[user_id].scores;
+  }  
+
+  get movieIdList() {
+    return Object.keys(this.movies);
+  }
+
+  get allUsers() {
+    return this.users;
+  }
+
+  get defaultScores() {
+    let user_id = this.userIdList[0];
+    return this.users[user_id].scores;
+  }
+
+  get nextScore() {
+    return Object.keys(this.defaultScores).length;
+  }
+
+  getUserById(id) {
+    id = parseInt(id);
+    return this.users[id];
+  }
+
+  getMovieById(id) {
+    id = parseInt(id);
+    return this.movies[id]
+  }
+
+  getUserScores(id) {
+    id = parseInt(id);
+    return this.users[id].scores;
+  }
+
+  getMovieByTitle(title) {
+
+  }
+
+
+
+  //// Delete
+
+  deleteUser(id_to_delete) {
+    // TODO: delete user's scores from users and movies
+    this.users.delete(id_to_delete);
+    console.warn("This feature not fully implemented. Scores remain.");
+    
+  }
+
+  deleteMovie(id_to_delete) {
+    // TODO: delete movies's scores from users and movies
+    this.movies.delete(id_to_delete);
+    console.warn("This feature not fully implemented. Scores remain.");
+  }
+
+  //// Add
+
+  addMovie(movie_title) {
+    if (! this.movieTitleExists(movie_title)) {
+      let movie_id = this.nextId;
+      let new_movie = {title: movie_title, scores: {}};
+      this.movies[movie_id] = new_movie;
+      this.addNewMovieScoreToUsers(movie_id);
+      return movie_id;
+    }
+    throw "Movie title already exists";
   }
 
   addUser(new_username) {
     if (! this.userNameExists(new_username) ) {
-      let user_id = this.createNewUserId();
-      let scores = this.createDefaultScores();
-      let new_user = {id: user_id, name: new_username, scores: scores};
-      this.db.users.push(new_user);
-      return true;
+      let user_id = this.nextId;
+      let scores = this.defaultScores;
+      let new_user = {name: new_username, scores: scores};
+      this.users[user_id] = new_user;
+      return user_id;
     } else {
-      return false; 
+      throw "Could not add user id!";
     }
   }
 
-  removeUser(id_to_remove) {
-    for (var i = this.db.users.length - 1; i >= 0; i--) {
-      if (this.db.users[i].id === id_to_remove) {
-        this.db.users.splice(i,1);
-        return true;
-      } 
-    }
-    return false;
-  }
-
-  addMovie(movie_title) {
-    if (! this.movieTitleExists(movie_title)) {
-      let movie_id = this.createNewMovieId();
-      let new_movie = {id: movie_id, title: movie_title};
-      this.db.movies.push(new_movie);
-      this.addNewMovieScoreToUsers(movie_id);
-      return true;
-    }
-    return false;
-  }
-
-  removeMovie(id_to_remove) {
-    for (var i = this.db.movies.length - 1; i >= 0; i--) {
-      if (this.db.movies[i].id === parseInt(id_to_remove)) {
-        this.db.movies.splice(i,1);
-        return true;
-      } 
-    }
-    return false;
-  }
-
-  renameUser(user_id, new_username) {
-    for (var i = this.db.users.length - 1; i >= 0; i--) {
-      if (this.db.users[i].id === parseInt(user_id)) {
-        this.db.users[i].name = new_username;
-      } 
-      return true;
-    }
-    return false;
-  }
-
-  renameMovie(movie_id, new_title) {
-    for (var i = this.db.movies.length - 1; i >= 0; i--) {
-      if (this.db.movies[i].id === movie_id) {
-        this.db.movies[i].title = new_title;
-        return true;
-      } 
-    }
-    return false;
-  }
-
-  userIdList() {
-    return this.db.users.map(user => user.id);
-  }
-
-  movieIdList() {
-    return this.db.movies.map(movie => movie.id);
-  }
-
-  userNameExists(username) {
-    return Object.values(this.db.users).includes(username);
-  }
-
-  movieTitleExists(movie_title) {
-    return Object.values(this.db.movies).includes(movie_title);
-  }
-
-  createNewUserId() {
-    for (var i = this.db.users.length; i >= 0; i--) {
-      if (! Object.values(this.userIdList).includes(i)) {
-        return i;
-      }
-    }
-    return this.db.users.length;
-  }
-
-  createNewMovieId() {
-    for (var i = this.db.movies.length - 1; i >= 0; i--) {
-      if (! Object.values(this.movieIdList()).includes(i)) {
-        return i;
-      }
-    }
-    return this.db.movies.length;
-  }
-
-  createDefaultScores() {
-    return this.db.users[0].scores;
+  setMovieScore(user_id, movie_id, new_score) {
+    user_id = parseInt(user_id)
+    movie_id = parseInt(movie_id)
+    new_score = parseInt(new_score)
+    this.users[user_id].scores[movie_id] = new_score;
+    this.movies[movie_id].scores[user_id] = new_score;
   }
 
   addNewMovieScoreToUsers(movie_id) {
-    for (var i = this.db.users.length - 1; i >= 0; i--) {
-      let new_score = keys(this.db.users[i].scores).length;
-      this.db.users[i].scores[movie_id] = new_score;
+    movie_id = parseInt(movie_id)
+    let new_score = this.nextScore;
+
+    for (uid in this.users) {
+      this.setMovieScore(uid, movie_id, new_score)
     }
-    return true;
   }
+
+  //// Update
+
+  renameUser(user_id, new_username) {
+
+  }
+
+  renameMovie(movie_id, new_title) {
+
+  }
+
+  userNameExists(username) {
+    for (id in this.users) {
+      if (this.users[id].name === username ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  movieTitleExists(movie_title) {
+    for (id in this.movies) {
+      if (this.movies[id].name === movie_title ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
 
 } // end Class
 
